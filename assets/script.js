d1e1652548b9394ce7c05f0e6abcba9a19226049
dds git <deeann@stelzl.us>
2022-08-24 05:27:30 -0400

    set timer to display on page

--- assets/script.js
+++ assets/script.js
@@ -3,100 +3,108 @@ var start = document.getElementById("start");
 var quiz = document.getElementById("quiz");
 var questionsEl = document.getElementById("questions");
 var end = document.getElementById("end");
-var startButton = document.getElementById("start-button")
-var currentQuestionEl = document.getElementById("title")
+var startButton = document.getElementById("start-button");
+var currentQuestionEl = document.getElementById("title");
+var choice1 = document.getElementById("choice1");
+var choice2 = document.getElementById("choice2");
+var choice3 = document.getElementById("choice3");
+var choice4 = document.getElementById("choice4");
+var submitInitialsButton = document.getElementById("submit-initials");
+var finalScreen = document.getElementById("final-screen");
+var clock = document.getElementById("clock");
 
-var choice1 = document.getElementById("choice1")
-var choice2 = document.getElementById("choice2")
-var choice3= document.getElementById("choice3")
-var choice4 = document.getElementById("choice4")
-
-var questionIndex ;
-var userScore
+var questionIndex;
+var userScore;
 //Set up objects/arrays for questions
-var questions = [{
-    quizQuestion : 'This is the text for Q1',
-    choices : [ 'correctAnswer', 'answer2', 'answer3', 'answer4'],
-    correctAnswer : 'correctAnswer'
-},
-
- {
-    quizQuestion: 'This is the text for Q2',
-    choices : [ 'answer1', 'answer2', 'answer3', 'correctAnswer'], 
-    correctAnswer : 'correctAnswer'
-},
-
+var questions = [
   {
-    quizQuestion : 'This is the text for Q3',
-    choices : [ 'answer1', 'correctAnswer', 'answer3', 'answer4'],
-    correctAnswer : 'correctAnswer'
-},
-
+    quizQuestion: "What is the name of Phoebe's twin sister?",
+    choices: ["Ursula", "Kate", "Monica", "Lisa"],
+    correctAnswer: "Ursula",
+  },
   {
-    quizQuestion: 'This is the text for Q4',
-    choices : [ 'answer1', 'answer2', 'correctAnswer', 'answer4'] ,
-    correctAnswer : 'correctAnswer'
-}]
-//
-
-
-function startQuiz(){
-  start.style.display = 'none';
-  quiz.style.display = 'block';
+    quizQuestion: "How many sisters does Joey have?",
+    choices: ["5", "11", "3", "7"],
+    correctAnswer: "7",
+  },
+  {
+    quizQuestion: "Where did Ross and Rachel have their first date?",
+    choices: ["The Zoo", "The Planetarium", "Central Park", "Empire State Building"],
+    correctAnswer: "The Planetarium",
+  },
+  {
+    quizQuestion: "Who is the youngest FRIEND?",
+    choices: ["Ross", "Chandler", "Rachel", "Joey"],
+    correctAnswer: "Rachel",
+  },
+];
+//Set up function to move from the start id display to the quiz id display
+function startQuiz() {
+  start.style.display = "none";
+  quiz.style.display = "block";
   questionIndex = 0;
   userScore = 0;
-  //start timer
   displayQuestions();
 }
+//Set up function to sort questions by index
 function displayQuestions() {
-currentQuestionEl.textContent = questions[questionIndex].quizQuestion;
-choice1.textContent = questions[questionIndex].choices[0];
-choice2.textContent = questions[questionIndex].choices[1];
-choice3.textContent = questions[questionIndex].choices[2];
-choice4.textContent = questions[questionIndex].choices[3]
+  currentQuestionEl.textContent = questions[questionIndex].quizQuestion;
+  choice1.textContent = questions[questionIndex].choices[0];
+  choice2.textContent = questions[questionIndex].choices[1];
+  choice3.textContent = questions[questionIndex].choices[2];
+  choice4.textContent = questions[questionIndex].choices[3];
 }
-
-function checkAnswer(event){
+//Set up function to check if the answer selected is the correct answer
+function checkAnswer(event) {
   console.log(event);
   if (event.target.className === "choice") {
-    if (event.target.textContent === questions[questionIndex].correctAnswer){
-      console.log('correct');
+    if (event.target.textContent === questions[questionIndex].correctAnswer) {
+      console.log("correct");
       userScore += 10;
-    }else {
-      console.log('incorrect');
+    } else {
+      console.log("incorrect");
     }
-    questionIndex++
+    questionIndex++;
     if (questionIndex === questions.length) {
-      endGame()
-    }else{
-      displayQuestions()
+      endGame();
+    } else {
+      displayQuestions();
     }
+  }
+}
+//Set up function to change display from questions to user score
+function endGame() {
+  quiz.style.display = "none";
+  end.style.display = "block";
+  document.getElementById("user-score").textContent =
+    "Your Score Was " + userScore;
     
-  }
 }
-function endGame (){
-  quiz.style.display = 'none'; 
-  end.style.display = 'block';
-  document.getElementById ("user-score").textContent="Your Score Was " + userScore;
-}
-
-
-
-//Timer Function
-var secondsLeft = 45;
+var timer = setTime();
+var secondsLeft = 45
 function setTime() {
-    var quizCountdown = setInterval(function() {
-     console.log(secondsLeft);
-     secondsLeft--
-     if (secondsLeft === 0) {
-        console.log('Time is up!');
-        clearInterval(quizCountdown);
-     }
+  var quizCountdown = setInterval(function () {
+    console.log(secondsLeft);
+    secondsLeft--;
+    clock.textContent = secondsLeft
+    
+
+    if (secondsLeft === 0) {
+      console.log("Time is up!");
+      clearInterval(quizCountdown);
     }
-    , 1000);
-  }
-  //setTime();   //uncomment when running
-  questionsEl.addEventListener('click', checkAnswer)
- 
-  startButton.addEventListener('click', startQuiz)
-  //questionEl.addEventListener('click', )
+  }, 1000);
+}
+//setTime();   //uncomment when running
+
+
+//creating dynamic user event to check for correct answer upon user clicking a choice
+questionsEl.addEventListener("click", checkAnswer);
+//set start button to activate quiz when user clicks button
+startButton.addEventListener("click", startQuiz);
+
+//Math.floor((distance % (1000 * 60))/1000);
+//document.getElementById("clock").innerHTML = secondsLeft;
+
+
+
